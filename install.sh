6096e4ac7be45a7c17ce8393e3f834bc31e57389
Lennard Indlekofer <info@lennard7001.com>
2020-06-14 05:11:47 +0200

    add autoexec configuration

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "gamemode ${GMOD_GAMEMODE}" > ${STEAM_APP_DIR}/garrysmod/cfg/autoexec.cfg
+
 ${STEAM_CMD_DIR}/steamcmd.sh +login anonymous +force_install_dir ${STEAM_APP_DIR} +app_update 4020 +quit
 
 if [ ${GMOD_CSS} = true ] && [ ${GMOD_TF2} = true ]; then
